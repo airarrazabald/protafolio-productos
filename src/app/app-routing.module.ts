1906f493027d4dba814e5d873e4cff4999ac6bdf
Abelardo Irarrazabal <a.irarrazabal.d@gmail.com>
2018-09-21 12:39:38 -0300

    Fin Seccion 3

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,7 @@ import { AboutComponent } from './pages/about/about.component';
 import { ItemComponent } from './pages/item/item.component';
 
 const app_routes: Routes = [
-    {path: '', component: PortafolioComponent},
+    {path: 'home', component: PortafolioComponent},
     {path: 'about', component: AboutComponent},
     {path: 'item', component: ItemComponent},
     {path: '**', pathMatch: 'full' , redirectTo: ''}
@@ -13,7 +13,7 @@ const app_routes: Routes = [
 
 
 @NgModule({
-    imports: [ RouterModule.forRoot(app_routes) ],
+    imports: [ RouterModule.forRoot(app_routes , {useHash: true} /* Uso de hash para rutas en angular le agrega "#" a una ruta */ ) ],
     exports: [ RouterModule]
 })
 export class AppRoutingModule {
